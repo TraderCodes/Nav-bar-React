569318494fa420054ab715fdb39407f94b8dddae
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-05-13 02:13:28 -0700

    Project start

---
+++ src/Navbar.js
@@ -0,0 +1,24 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { FaBars, FaTwitter } from 'react-icons/fa';
+import { links, social } from './data';
+import logo from './logo.svg';
+
+const Navbar = () => {
+   return (
+      <nav>
+         <div className="nav-center">
+            <div className="nav-header">
+               <img src={logo} alt="logo" />
+               <button className="nav-toggle">
+                  <FaBars />
+               </button>
+            </div>
+
+            <div className="links-con`tainer show-container"></div>
+            <ul className="social-icons"></ul>
+         </div>
+      </nav>
+   );
+};
+
+export default Navbar;
