be97aa340072ae0a6ce19c386b2310cd7f3b3152
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-05-13 03:26:30 -0700

    ✔ quick toggle to show links

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,18 +4,48 @@ import { links, social } from './data';
 import logo from './logo.svg';
 
 const Navbar = () => {
+   // 【set to show links when clicked 】
+   const [showlinks, setShowLinks] = useState(false);
    return (
       <nav>
          <div className="nav-center">
             <div className="nav-header">
                <img src={logo} alt="logo" />
-               <button className="nav-toggle">
+               <button
+                  className="nav-toggle"
+                  onClick={() => setShowLinks(!showlinks)}
+               >
                   <FaBars />
                </button>
             </div>
+            {/* if showlink is true show the container */}
+            {showlinks && (
+               <div className="links-container show-container">
+                  <ul className="links">
+                     {links.map((link) => {
+                        // destructure
+                        const { id, url, text } = link;
+                        return (
+                           <li key={id}>
+                              <a href={url}>{text}</a>
+                           </li>
+                        );
+                     })}
+                  </ul>
+               </div>
+            )}
 
-            <div className="links-con`tainer show-container"></div>
-            <ul className="social-icons"></ul>
+            <ul className="social-icons">
+               {/* destructure */}
+               {social.map((social) => {
+                  const { id, url, icon } = social;
+                  return (
+                     <li key={id}>
+                        <a href={url}>{icon} </a>
+                     </li>
+                  );
+               })}
+            </ul>
          </div>
       </nav>
    );
